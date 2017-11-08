4bf66d7da500c1ae9a29e2699d291aaab6c13796
Heather Creighton <heather.creighton@gmail.com>
2017-11-08 14:57:21 -0500

    first commit

---
+++ app/controllers/practice_controller.rb
@@ -0,0 +1,10 @@
+class PracticeController < ApplicationController
+  def index
+ 		@name = params[:name]
+  end
+
+  def about
+  
+  	@color = params[:color]
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+
+
+  root 'posts#index'
+  get 'index' =>'practice#index'
+  post 'index' => 'practice#index'
+  get 'about' => 'practice#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
