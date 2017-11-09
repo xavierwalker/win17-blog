5ccc675a5d935115d8649b8eecdd00de31b862f7
Heather Creighton <heather.creighton@gmail.com>
2017-11-09 18:35:49 -0500

    added carrierwave

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -69,7 +69,7 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:author, :comment_entry, :post_id)
+      params.require(:comment).permit(:author, :comment_entry, :post_id, :user_id)
     end
 end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,7 +70,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :author, :blog_entry)
+      params.require(:post).permit(:title, :author, :blog_entry, :user_id)
     end
 end
 
